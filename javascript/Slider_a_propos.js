d63a15323a0d8985d3ae69d8edacf972ca13719a
bernadette chevalier <chevalierb59@gmail.com>
2025-01-16 16:35:32 +0100

    modifapropos

--- javascript/Slider_a_propos.js
+++ javascript/Slider_a_propos.js
@@ -1,1 +1,12 @@
+const rightArrow = document.querySelector('#imgarrow');
+const slide = document.querySelector('.slider');
+const leftArrow = document.querySelector('#leftarrow');
 
+rightArrow.addEventListener('click', ()=>{
+  slide.style.transform = 'translateX(-100vw)';
+})
+
+leftArrow.addEventListener('click',()=>{
+  slide.style.transform = 'translateX(0)';
+  console.log('Salut');
+})
